c45611118c5f4416df073b12c6155c4466e1fb1b
JRCcoding <jrccode@outlook.com>
2023-02-15 18:38:16 -0600

    rough setup before concurrently script, server

---
+++ client/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h2>To-Do List</h2>
+      <h6>Made with React.js by Josh Claxton</h6>
+    </div>
+  )
+}
+
+export default Header
